009876bb38589f37f106a1c60cc96ecced0dd75d
lukassemmler <kontakt@lukassemmler.de>
2022-02-27 00:10:47 +0100

    Add cv records to 'about'

--- pages/about.vue
+++ pages/about.vue
@@ -10,7 +10,59 @@
         :timespan="$t('text_about_work-experience-1_time-span')"
         :location="$t('text_about_work-experience-1_location')"
         :description="$t('text_about_work-experience-1_description').join('')"
-      ><template v-slot:extra>Test</template></cv-record>
+      ></cv-record>
+
+      <cv-record
+        iconId="business"
+        :level="0"
+        :title="$t('text_about_work-experience-2_organization')"
+        :subtitle="$t('text_about_work-experience-2_title')"
+        :timespan="$t('text_about_work-experience-2_time-span')"
+        :location="$t('text_about_work-experience-2_location')"
+        :description="$t('text_about_work-experience-2_description').join('')"
+      ></cv-record>
+
+      <cv-record
+        iconId="school"
+        :level="0"
+        :title="$t('text_about_education-1_organization')"
+        :subtitle="$t('text_about_education-1_title')"
+        :timespan="$t('text_about_education-1_time-span')"
+        :location="$t('text_about_education-1_location')"
+        :description="$t('text_about_education-1_description').join('')"
+      ></cv-record>
+
+      <cv-record
+        iconId="school"
+        :level="1"
+        :title="$t('text_about_education-2_organization')"
+        :subtitle="$t('text_about_education-2_title')"
+        :timespan="$t('text_about_education-2_time-span')"
+        :location="$t('text_about_education-2_location')"
+        :description="$t('text_about_education-2_description').join('')"
+        ><template v-slot:extra
+          ><!-- TODO add button --><button>PDF</button></template
+        ></cv-record
+      >
+
+      <cv-record
+        iconId="trophy"
+        :level="1"
+        :title="$t('text_about_education-3_organization')"
+        :subtitle="$t('text_about_education-3_title')"
+        :timespan="$t('text_about_education-3_time-span')"
+        :location="$t('text_about_education-3_location')"
+        :description="$t('text_about_education-3_description').join('')"
+      ></cv-record>
+
+      <cv-record
+        iconId="trophy"
+        :level="1"
+        :title="$t('text_about_education-4_organization')"
+        :subtitle="$t('text_about_education-4_title')"
+        :timespan="$t('text_about_education-4_time-span')"
+        :location="$t('text_about_education-4_location')"
+      ></cv-record>
     </div>
   </div>
 </template>
