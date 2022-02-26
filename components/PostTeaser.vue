092335c0aabc5efd8a5552b4116098468a869422
lukassemmler <kontakt@lukassemmler.de>
2022-02-26 23:10:16 +0100

    Add option 'horizontal' to PostTeaser

--- components/PostTeaser.vue
+++ components/PostTeaser.vue
@@ -1,35 +1,39 @@
 <template>
   <article class="post-teaser">
-    <nuxt-link :to="link" :title="linkTitle" class="post-teaser-link">
-      <simple-image v-bind="$attrs" />
-    </nuxt-link>
-    <header class="post-teaser-header">
-      <h3 class="post-teaser-title">
-        <nuxt-link
-          class="invisible-link"
-          :to="link"
-          :title="linkTitle"
-          v-html="title"
-        >
-        </nuxt-link>
-      </h3>
-      <p class="post-teaser-subtitle" v-if="subtitle">
-        {{ subtitle }}
+    <aside class="post-teaser-figure">
+      <nuxt-link :to="link" :title="linkTitle" class="post-teaser-link">
+        <simple-image v-bind="$attrs" />
+      </nuxt-link>
+    </aside>
+    <main class="post-teaser-content">
+      <header class="post-teaser-header">
+        <div class="post-teaser-tags" v-if="tags">
+          <ul class="post-teaser-tags-list">
+            <li v-for="tag in tags" :key="tag">
+              <simple-tag :text="tag"></simple-tag>
+            </li>
+          </ul>
+        </div>
+        <h3 class="post-teaser-title">
+          <nuxt-link
+            class="invisible-link"
+            :to="link"
+            :title="linkTitle"
+            v-html="title"
+          >
+          </nuxt-link>
+        </h3>
+        <p class="post-teaser-subtitle" v-if="subtitle">
+          {{ subtitle }}
+        </p>
+      </header>
+      <p v-if="description" class="post-teaser-description">
+        <span v-html="description"></span>
+        <nuxt-link :to="link" :title="linkTitle">{{
+          $t("label_continue-reading")
+        }}</nuxt-link>
       </p>
-    </header>
-    <p v-if="description" class="post-teaser-description">
-      <span v-html="description"></span>
-      <nuxt-link :to="link" :title="linkTitle">{{
-        $t("label_continue-reading")
-      }}</nuxt-link>
-    </p>
-    <div class="post-teaser-tags" v-if="tags">
-      <ul class="post-teaser-tags-list">
-        <li v-for="tag in tags" :key="tag">
-          <simple-tag :text="tag"></simple-tag>
-        </li>
-      </ul>
-    </div>
+    </main>
   </article>
 </template>
 
@@ -77,13 +81,42 @@ export default {
 
 <style lang="scss" scoped>
 .post-teaser {
-  margin-bottom: $sp;
+  margin-bottom: 3 * $sp;
 
   &.centered {
     text-align: center;
 
     .post-teaser-tags-list {
       justify-content: center;
+    }
+  }
+
+  &.horizontal {
+    @include breakpoint-upwards($breakpoint-semi-big) {
+      display: flex;
+      flex-direction: row;
+
+      .post-teaser-figure {
+        flex: 1 1;
+        margin-right: 1.5rem;
+        margin-bottom: 0;
+      }
+
+      .post-teaser-content {
+        flex: 3 3;
+      }
+
+      .post-teaser-link {
+        &:hover,
+        &:focus {
+          transform: none;
+          box-shadow: none;
+        }
+      }
+
+      .post-teaser-description {
+        max-width: 50rem;
+      }
     }
   }
 }
@@ -99,13 +132,17 @@ export default {
   line-height: 1.25;
   margin-top: 0;
   margin-bottom: 1rem;
-  opacity: 0.5;
+  color: $gray-60;
 }
 
 .post-teaser-header {
   margin-bottom: 1.5rem;
 
-  & >:last-child{
+  & > *:first-child {
+    margin-top: 0;
+  }
+
+  & > *:last-child {
     margin-bottom: 0;
   }
 }
@@ -115,9 +152,12 @@ export default {
   margin-top: 0;
 }
 
+.post-teaser-figure {
+  margin-bottom: 1.5rem;
+}
+
 .post-teaser-link {
   display: block;
-  margin-bottom: 1.5rem;
   transition: transform 0.1s ease-in-out, box-shadow 0.1s ease-in-out;
 
   img {
@@ -132,10 +172,17 @@ export default {
   }
 }
 
+.post-teaser-tags {
+  font-size: 0.9em;
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+}
+
 .post-teaser-tags-list {
   list-style-type: none;
   display: flex;
   flex-flow: row wrap;
+  margin: 0;
   padding: 0;
 
   & > *:not(:last-child) {
