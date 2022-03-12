d8b51d8b5ad19c48673feec94feb00b25a592923
lukassemmler <kontakt@lukassemmler.de>
2022-03-12 11:52:07 +0100

    Add layout 'vanilla'

---
+++ layouts/vanilla.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="content">
+    <page-header></page-header>
+    <Nuxt />
+    <page-footer></page-footer>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import "~/assets/style/_general.scss";
+@import "~/assets/style/_layout-container.scss";
+@import "~/assets/style/_layout-flex.scss";
+@import "~/assets/style/_layout-grid.scss";
+@import "~/assets/style/_layout-column.scss";
+@import "~/assets/style/_button.scss";
+</style>
