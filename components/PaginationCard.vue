77cddadce29865f9c033a574060b19028a847849
lukassemmler <kontakt@lukassemmler.de>
2022-03-14 22:21:32 +0100

    Add components 'PaginationCard' and 'ProjectPagination'

---
+++ components/PaginationCard.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="pagination-card" :class="[color, direction]">
+    <nuxt-link :to="link" class="pagination-card-link">
+      <div class="pagination-card-inner">
+        <span class="pagination-card-arrow" :class="arrowDirection">
+          <component v-bind:is="arrowDirection + '-arrow'"></component>
+        </span>
+        <span class="pagination-card-label">{{ label }}</span>
+        <span class="pagination-card-title">{{ title }}</span>
+      </div>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+import LeftArrow from "~/assets/icons/material-west.svg?inline";
+import RightArrow from "~/assets/icons/material-east.svg?inline";
+export default {
+  components: {LeftArrow, RightArrow},
+  props: {
+    title: String,
+    label: String,
+    link: String,
+    color: String,
+    direction: {
+      type: String,
+      required: true,
+      default: "next",
+      validator: function (value) {
+        return ["previous", "next"].includes(value);
+      },
+    },
+  },
+  computed: {
+    arrowDirection: function () {
+      const { direction } = this;
+      if (direction === "next") return "right";
+      if (direction === "previous") return "left";
+      throw new Error(`Unsupported arrow direction '${direction}'. `);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.pagination-card {
+  line-height: 1.2;
+  letter-spacing: 0.02em;
+  color: $white;
+  background-color: $gray-40;
+  transition: background-color 0.1s ease-in;
+
+  &:hover,
+  &:focus,
+  &:focus-within {
+    background-color: $gray-50;
+
+    .pagination-card-arrow {
+      &:not(.right) {
+        transform: translate3d(-20%, 0, 0);
+      }
+      &.right {
+        transform: translate3d(20%, 0, 0);
+      }
+    }
+  }
+
+  &.previous {
+    .pagination-card-inner {
+      margin-left: auto;
+    }
+  }
+
+  &.next {
+    .pagination-card-inner {
+      align-items: flex-end;
+      text-align: right;
+      padding: 2em 3.5em 2em 1em;
+    }
+    .pagination-card-arrow {
+      left: auto;
+      right: 1em;
+    }
+  }
+
+  @include breakpoint-upwards($breakpoint-semi-big) {
+    .project-pagination-inner {
+      max-width: $max-size-wrapper * 0.5;
+    }
+  }
+}
+
+.pagination-card-inner {
+  position: relative;
+  box-sizing: border-box;
+  padding: 2em 1em 2em 3.5em;
+}
+
+.pagination-card-link {
+  display: block;
+  border: none;
+  text-decoration: none;
+  color: $white;
+}
+
+.pagination-card-title {
+  display: block;
+  font-size: 2em;
+  font-weight: 700;
+}
+
+.pagination-card-label {
+  display: block;
+  font-size: 0.8em;
+  text-transform: uppercase;
+  color: $bright-80;
+  margin-bottom: 0.4em;
+}
+
+.pagination-card-arrow {
+  position: absolute;
+  top: 3.55em;
+  left: 1em;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  width: 2em;
+  height: 2em;
+  color: $white;
+  transition: transform 0.1s ease-in;
+
+  svg {
+    width: 100%;
+    height: 100%;
+    fill: currentColor;
+  }
+}
+</style>
---
+++ components/ProjectPagination.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav class="project-pagination">
+    <ul class="project-pagination-list">
+      <li class="project-pagination-item" v-if="previousTitle">
+        <pagination-card
+          :title="previousTitle"
+          :color="previousColor"
+          :link="localePath(previousLink)"
+          :label="$t('label_project-pagination_previous')"
+          direction="previous"
+        ></pagination-card>
+      </li>
+      <li class="project-pagination-item" v-if="nextTitle">
+        <pagination-card
+          :title="nextTitle"
+          :color="nextColor"
+          :link="localePath(nextLink)"
+          :label="$t('label_project-pagination_next')"
+          direction="next"
+        ></pagination-card>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import PaginationCard from "./PaginationCard.vue";
+
+export default {
+  components: { PaginationCard },
+  props: {
+    previousTitle: String,
+    previousColor: String,
+    previousLink: String,
+    nextTitle: String,
+    nextColor: String,
+    nextLink: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.project-pagination-list {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  display: block;
+  flex-flow: row nowrap;
+  align-items: stretch;
+
+  @media screen and (min-width: $breakpoint-semi-big) {
+    .project-pagination-list {
+      display: flex;
+    }
+  }
+}
+
+.project-pagination-item {
+  flex: 1 1 0%;
+
+  & > * {
+    height: 100%;
+  }
+}
+</style>
