2df812b04d2fac362056d10df53b6f73d84e4a02
lukassemmler <kontakt@lukassemmler.de>
2022-03-14 22:22:20 +0100

    Add pagination to page 'design-booklet'

--- pages/projects/design-booklet.vue
+++ pages/projects/design-booklet.vue
@@ -1,13 +1,21 @@
 <template>
-  <h1>Design-booklet</h1>
+  <div>
+    <h1>Design-booklet</h1>
+    <!-- TODO Fix links in project pagination -->
+    <project-pagination
+      :previousTitle="$t('project-nav_data-visualization')"
+      previousLink="projects/data-visualization"
+      previousColor="bg-hover-blue"
+      :nextTitle="$t('project-nav_3d-modelling')"
+      nextLink="projects/3d-modelling"
+      nextColor="bg-hover-pink"
+    ></project-pagination>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
