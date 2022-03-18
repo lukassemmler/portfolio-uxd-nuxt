9cf296d584291a1facfa810365f45e03837cb36f
lukassemmler <kontakt@lukassemmler.de>
2022-03-18 20:35:28 +0100

    Add template 'project' to 'design booklet'

--- pages/projects/design-booklet.vue
+++ pages/projects/design-booklet.vue
@@ -1,20 +1,27 @@
 <template>
-  <div>
-    <h1>Design-booklet</h1>
-    <!-- TODO Fix links in project pagination -->
-    <project-pagination
-      :previousTitle="$t('project-nav_data-visualization')"
-      previousLink="projects/data-visualization"
-      previousColor="bg-hover-blue"
-      :nextTitle="$t('project-nav_3d-modelling')"
-      nextLink="projects/3d-modelling"
-      nextColor="bg-hover-pink"
-    ></project-pagination>
-  </div>
+  <project-template>
+    <template v-slot:header>
+      <h1>Design-booklet</h1>
+    </template>
+    <template v-slot:content>
+      <!-- TODO Fix links in project pagination -->
+      <project-pagination
+        :previousTitle="$t('project-nav_data-visualization')"
+        previousLink="projects/data-visualization"
+        previousColor="bg-hover-blue"
+        :nextTitle="$t('project-nav_3d-modelling')"
+        nextLink="projects/3d-modelling"
+        nextColor="bg-hover-pink"
+      ></project-pagination>
+    </template>
+  </project-template>
 </template>
 
 <script>
-export default {};
+import ProjectTemplate from "~/templates/project.vue";
+export default {
+  components: { ProjectTemplate },
+};
 </script>
 
 <style lang="scss" scoped>
