9999bfbc7863e5323e9b9904098aa566728189d2
lukassemmler <kontakt@lukassemmler.de>
2022-03-19 14:36:43 +0100

    Add text to page 'imprint'

--- pages/imprint.vue
+++ pages/imprint.vue
@@ -1,5 +1,66 @@
 <template>
-  <h1>Imprint</h1>
+  <div>
+    <header>
+      <div class="container-small">
+        <h1>{{ $t("heading_imprint") }}</h1>
+      </div>
+    </header>
+
+    <section id="section-address" class="section">
+      <div class="container-small">
+        <h2 id="address">{{ $t("heading_imprint_address") }}</h2>
+        <div
+          class="text"
+          v-markdown="$t('text_imprint_address').join('')"
+        ></div>
+      </div>
+    </section>
+
+    <section id="section-contact" class="section">
+      <div class="container-small">
+        <h2 id="contact">{{ $t("heading_imprint_contact") }}</h2>
+        <div
+          class="text"
+          v-markdown="$t('text_imprint_contact').join('')"
+        ></div>
+      </div>
+    </section>
+
+    <section id="section-accountability-for-content" class="section">
+      <div class="container-small">
+        <h2 id="accountability-for-content">
+          {{ $t("heading_imprint_accountability-for-content") }}
+        </h2>
+        <div
+          class="text"
+          v-markdown="$t('text_imprint_accountability-for-content').join('')"
+        ></div>
+      </div>
+    </section>
+
+    <section id="section-accountability-for-links" class="section">
+      <div class="container-small">
+        <h2 id="accountability-for-links">
+          {{ $t("heading_imprint_accountability-for-links") }}
+        </h2>
+        <div
+          class="text"
+          v-markdown="$t('text_imprint_accountability-for-links').join('')"
+        ></div>
+      </div>
+    </section>
+
+    <section id="section-copyright" class="section">
+      <div class="container-small">
+        <h2 id="copyright">{{ $t("heading_imprint_copyright") }}</h2>
+        <div
+          class="text"
+          v-markdown="$t('text_imprint_copyright').join('')"
+        ></div>
+        <p v-markdown="$t('text_imprint_source')"></p>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
