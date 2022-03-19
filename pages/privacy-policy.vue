c995f63aed2c7d4be9bf7d19a99b24fbb700276b
lukassemmler <kontakt@lukassemmler.de>
2022-03-19 14:17:55 +0100

    Add content to page 'privacy-policy'

--- pages/privacy-policy.vue
+++ pages/privacy-policy.vue
@@ -1,6 +1,34 @@
 <template>
   <div>
-    <h1>Privacy Policy</h1>
+    <header>
+      <div class="container-small">
+        <h1 v-html="$t('heading_privacy-policy')"></h1>
+      </div>
+    </header>
+    
+    <section id="section-data-protection" class="section">
+      <div class="container-small">
+        <h2
+          id="data-protection"
+          v-html="$t('heading_privacy-policy_data-protection')"
+        ></h2>
+        <div
+          class="text"
+          v-markdown="$t('text_privacy-policy_data-protection').join('')"
+        ></div>
+      </div>
+    </section>
+
+    <section id="section-log-files" class="section">
+      <div class="container-small">
+        <h2 id="log-files" v-html="$t('heading_privacy-policy_log-files')"></h2>
+        <div
+          class="text"
+          v-markdown="$t('text_privacy-policy_log-files').join('')"
+        ></div>
+        <p v-markdown="$t('text_privacy-policy_source')"></p>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -10,14 +38,14 @@ export default {
   layout: "vanilla",
   head() {
     return getMetaData({
-      url: this.$config.baseUrl + this.$route.path, 
-      siteName: this.$t("label_site-name"), 
-      title: this.$t("meta-title_privacy-policy"), 
-      titleTemplate: "%s \u2013 Lukas Semmler", 
-      description: this.$t("meta-description_privacy-policy"), 
-      keywords: this.$t("meta-keywords_privacy-policy"), 
-      author: this.$t("meta-author_privacy-policy"), 
-      thumbnail: "/thumbnail/Thumbnail Design Booklet.jpg", 
+      url: this.$config.baseUrl + this.$route.path,
+      siteName: this.$t("label_site-name"),
+      title: this.$t("meta-title_privacy-policy"),
+      titleTemplate: "%s \u2013 Lukas Semmler",
+      description: this.$t("meta-description_privacy-policy"),
+      keywords: this.$t("meta-keywords_privacy-policy"),
+      author: this.$t("meta-author_privacy-policy"),
+      thumbnail: "/thumbnail/Thumbnail Design Booklet.jpg",
       thumbnailAlt: this.$t("alt_thumbnail_general"),
     });
   },
