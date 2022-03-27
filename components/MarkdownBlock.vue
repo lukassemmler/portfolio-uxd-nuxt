60ed87627425bbe8ffacef201340385f734bdb4c
lukassemmler <kontakt@lukassemmler.de>
2022-03-27 22:27:40 +0200

    Add component 'MarkdownBlock'

---
+++ components/MarkdownBlock.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="markdown-block" v-html="markupProcessed"></div>
+</template>
+
+<script>
+import {
+  replacePlaceholders as renderPlaceholders,
+  replaceLinksWithLocalizedLinks as renderLocalizedLinks,
+} from "~/assets/lib/string-util";
+export default {
+  props: {
+    markup: {
+      type: String,
+      required: true,
+    },
+    format: {
+      type: Object,
+      required: false,
+      default: function () {
+        return {};
+      },
+    },
+  },
+  computed: {
+    markupProcessed: function () {
+      const { $md, localePath, placeholders, markup } = this;
+      if ($md === undefined)
+        throw new Error(
+          `Cannot find instance property '$md'. Make sure you have '@nuxtjs/markdownit' installed. `
+        );
+      if (localePath === undefined)
+        throw new Error(
+          `Cannot find instance function 'localePath()'. Make sure you have '@nuxtjs/i18n' installed. `
+        );
+      let formattedMarkup = markup;
+      if (placeholders !== null)
+        formattedMarkup = renderPlaceholders(formattedMarkup, placeholders);
+      formattedMarkup = renderLocalizedLinks(formattedMarkup, localePath);
+      console.log(formattedMarkup);
+      return $md.render(formattedMarkup);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
