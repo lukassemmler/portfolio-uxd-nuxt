c7ae6fb1569efaea4f3b04f80c8d3fe0fa486bea
lukassemmler <kontakt@lukassemmler.de>
2022-05-15 15:39:48 +0200

    Add about billboard to index

--- pages/index.vue
+++ pages/index.vue
@@ -1,87 +1,111 @@
 <template>
   <div class="container huge">
-    <div class="pillar-container gapped-h gapped-v">
-      <div class="pillar-row">
-        <div class="pillar-col-12 pillar-col-sbg-6 pillar-col-bg-4">
-          <post-teaser
-            class="centered"
-            link="/projects/power-ui"
-            :linkTitle="$t('project-link-title_power-ui')"
-            src="thumbnail/Thumbnail Power-UI.jpg"
-            :alt="$t('alt_thumbnail_power-ui')"
-            ratio="4-by-3"
-            sizes="xs:400px sm:640px md:960px lg:1200px"
-            :title="$t('project-title-raw_power-ui')"
-            :subtitle="$t('project-subtitle_power-ui')"
-          ></post-teaser>
-        </div>
-        <div class="pillar-col-12 pillar-col-sbg-6 pillar-col-bg-4">
-          <post-teaser
-            class="centered"
-            link="/projects/data-visualization"
-            :linkTitle="$t('project-link-title_data-visualization')"
-            src="thumbnail/Thumbnail Data Visualization.jpg"
-            :alt="$t('alt_thumbnail_data-visualization')"
-            ratio="4-by-3"
-            sizes="xs:400px sm:640px md:960px lg:1200px"
-            :title="$t('project-title-raw_data-visualization')"
-            :subtitle="$t('project-subtitle_data-visualization')"
-          ></post-teaser>
-        </div>
-        <div class="pillar-col-12 pillar-col-sbg-6 pillar-col-bg-4">
-          <post-teaser
-            class="centered"
-            link="/projects/design-booklet"
-            :linkTitle="$t('project-link-title_design-booklet')"
-            src="thumbnail/Thumbnail Design Booklet.jpg"
-            :alt="$t('alt_thumbnail_design-booklet')"
-            ratio="4-by-3"
-            sizes="xs:400px sm:640px md:960px lg:1200px"
-            :title="$t('project-title-raw_design-booklet')"
-            :subtitle="$t('project-subtitle_design-booklet')"
-          ></post-teaser>
-        </div>
-        <div class="pillar-col-12 pillar-col-sbg-6 pillar-col-bg-4">
-          <post-teaser
-            class="centered"
-            link="/projects/3d-modelling"
-            :linkTitle="$t('project-link-title_3d-modelling')"
-            src="thumbnail/Thumbnail 3D Modelling.jpg"
-            :alt="$t('alt_thumbnail_3d-modelling')"
-            ratio="4-by-3"
-            sizes="xs:400px sm:640px md:960px lg:1200px"
-            :title="$t('project-title-raw_3d-modelling')"
-            :subtitle="$t('project-subtitle_3d-modelling')"
-          ></post-teaser>
-        </div>
-        <div class="pillar-col-12 pillar-col-sbg-6 pillar-col-bg-4">
-          <post-teaser
-            class="centered"
-            link="/projects/steering-wheel"
-            :linkTitle="$t('project-link-title_steering-wheel')"
-            src="thumbnail/Thumbnail Steering Wheel.jpg"
-            :alt="$t('alt_thumbnail_steering-wheel')"
-            ratio="4-by-3"
-            sizes="xs:400px sm:640px md:960px lg:1200px"
-            :title="$t('project-title-raw_steering-wheel')"
-            :subtitle="$t('project-subtitle_steering-wheel')"
-          ></post-teaser>
-        </div>
-        <div class="pillar-col-12 pillar-col-sbg-6 pillar-col-bg-4">
-          <post-teaser
-            class="centered"
-            link="/projects/infotainment-system"
-            :linkTitle="$t('project-link-title_infotainment-system')"
-            src="thumbnail/Thumbnail Infotainment System.jpg"
-            :alt="$t('alt_thumbnail_infotainment-system')"
-            ratio="4-by-3"
-            sizes="xs:400px sm:640px md:960px lg:1200px"
-            :title="$t('project-title-raw_infotainment-system')"
-            :subtitle="$t('project-subtitle_infotainment-system')"
-          ></post-teaser>
+    <section>
+      <h2 class="sr-only">{{ $t("meta-title_about") }}</h2>
+      <simple-billboard
+        class="spaced-bottom"
+        src="lukas/lukas-waving.png"
+        :alt="$t('alt_design-booklet_front')"
+      >
+        <p>
+          <strong class="text-semi-big">Hallo!</strong> <br />Ich bin Lukas, ein
+          UX-Designer.
+        </p>
+        <simple-button
+          type="secondary"
+          prefixed-icon="east"
+          target="/about"
+          :alt="$t('meta-title_about')"
+          >{{ $t("meta-title_about") }}</simple-button
+        >
+      </simple-billboard>
+    </section>
+
+    <section>
+      <h2 class="sr-only">{{ $t("heading_index_projects") }}</h2>
+      <div class="pillar-container gapped-h gapped-v">
+        <div class="pillar-row">
+          <div class="pillar-col-12 pillar-col-sbg-6 pillar-col-bg-4">
+            <post-teaser
+              class="centered"
+              link="/projects/power-ui"
+              :linkTitle="$t('project-link-title_power-ui')"
+              src="thumbnail/Thumbnail Power-UI.jpg"
+              :alt="$t('alt_thumbnail_power-ui')"
+              ratio="4-by-3"
+              sizes="xs:400px sm:640px md:960px lg:1200px"
+              :title="$t('project-title-raw_power-ui')"
+              :subtitle="$t('project-subtitle_power-ui')"
+            ></post-teaser>
+          </div>
+          <div class="pillar-col-12 pillar-col-sbg-6 pillar-col-bg-4">
+            <post-teaser
+              class="centered"
+              link="/projects/data-visualization"
+              :linkTitle="$t('project-link-title_data-visualization')"
+              src="thumbnail/Thumbnail Data Visualization.jpg"
+              :alt="$t('alt_thumbnail_data-visualization')"
+              ratio="4-by-3"
+              sizes="xs:400px sm:640px md:960px lg:1200px"
+              :title="$t('project-title-raw_data-visualization')"
+              :subtitle="$t('project-subtitle_data-visualization')"
+            ></post-teaser>
+          </div>
+          <div class="pillar-col-12 pillar-col-sbg-6 pillar-col-bg-4">
+            <post-teaser
+              class="centered"
+              link="/projects/design-booklet"
+              :linkTitle="$t('project-link-title_design-booklet')"
+              src="thumbnail/Thumbnail Design Booklet.jpg"
+              :alt="$t('alt_thumbnail_design-booklet')"
+              ratio="4-by-3"
+              sizes="xs:400px sm:640px md:960px lg:1200px"
+              :title="$t('project-title-raw_design-booklet')"
+              :subtitle="$t('project-subtitle_design-booklet')"
+            ></post-teaser>
+          </div>
+          <div class="pillar-col-12 pillar-col-sbg-6 pillar-col-bg-4">
+            <post-teaser
+              class="centered"
+              link="/projects/3d-modelling"
+              :linkTitle="$t('project-link-title_3d-modelling')"
+              src="thumbnail/Thumbnail 3D Modelling.jpg"
+              :alt="$t('alt_thumbnail_3d-modelling')"
+              ratio="4-by-3"
+              sizes="xs:400px sm:640px md:960px lg:1200px"
+              :title="$t('project-title-raw_3d-modelling')"
+              :subtitle="$t('project-subtitle_3d-modelling')"
+            ></post-teaser>
+          </div>
+          <div class="pillar-col-12 pillar-col-sbg-6 pillar-col-bg-4">
+            <post-teaser
+              class="centered"
+              link="/projects/steering-wheel"
+              :linkTitle="$t('project-link-title_steering-wheel')"
+              src="thumbnail/Thumbnail Steering Wheel.jpg"
+              :alt="$t('alt_thumbnail_steering-wheel')"
+              ratio="4-by-3"
+              sizes="xs:400px sm:640px md:960px lg:1200px"
+              :title="$t('project-title-raw_steering-wheel')"
+              :subtitle="$t('project-subtitle_steering-wheel')"
+            ></post-teaser>
+          </div>
+          <div class="pillar-col-12 pillar-col-sbg-6 pillar-col-bg-4">
+            <post-teaser
+              class="centered"
+              link="/projects/infotainment-system"
+              :linkTitle="$t('project-link-title_infotainment-system')"
+              src="thumbnail/Thumbnail Infotainment System.jpg"
+              :alt="$t('alt_thumbnail_infotainment-system')"
+              ratio="4-by-3"
+              sizes="xs:400px sm:640px md:960px lg:1200px"
+              :title="$t('project-title-raw_infotainment-system')"
+              :subtitle="$t('project-subtitle_infotainment-system')"
+            ></post-teaser>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   </div>
 </template>
 
@@ -89,7 +113,10 @@
 import PageFooter from "~/components/PageFooter.vue";
 import PageHeader from "~/components/PageHeader.vue";
 import PostTeaser from "~/components/PostTeaser.vue";
+import SimpleBillboard from "~/components/SimpleBillboard.vue";
+import SimpleButton from "~/components/SimpleButton.vue";
 export default {
+  components: { SimpleBillboard, SimpleButton },
   layout: "vanilla",
 };
 </script>
