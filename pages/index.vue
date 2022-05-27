6bb7d2a1437d71feaa2d24bd87ef26b88879a35a
lukassemmler <kontakt@lukassemmler.de>
2022-05-27 14:58:18 +0200

    Remove dropdown button from index (was used for testing)

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,5 @@
 <template>
   <div class="container huge">
-    <dropdown-button
-      menu-id="lang-menu"
-      :menu="[
-        { label: $t('label_lang_const_german'), link: switchLocalePath('de') },
-        { label: $t('label_lang_const_english'), link: switchLocalePath('en') },
-      ]"
-      orientation="top-left"
-    ></dropdown-button>
-
     <section>
       <h2 class="sr-only">{{ $t("meta-title_about") }}</h2>
       <simple-billboard
