41cb080f10b2c3df2297ba9c24dbeee8fa4616cc
lukassemmler <kontakt@lukassemmler.de>
2022-05-29 15:37:38 +0200

    Change MobileMenu to be interactive and have animations

--- components/MobileMenu.vue
+++ components/MobileMenu.vue
@@ -1,38 +1,55 @@
 <template>
-  <div id="mobile-menu" class="mobile-menu" :class="{ inverted }">
-    <div class="mobile-menu-content">
-      <div class="mobile-menu-header container huge">
-        <lukas-logo :inverted="inverted"></lukas-logo>
-        <div class="mobile-menu-close flex-edge-right">
-          <simple-button
-            target="#"
-            type="inherit round big-icon square"
-            prefixedIcon="close"
-            :aria-label="$t('component_banner_close')"
-            role="button"
-          ></simple-button>
+  <transition name="fade-top">
+    <div
+      id="mobile-menu"
+      class="mobile-menu"
+      :class="{ inverted, visible }"
+      ref="mobileMenu"
+    >
+      <div class="mobile-menu-content">
+        <div class="mobile-menu-header container huge">
+          <lukas-logo :inverted="inverted" ref="logo"></lukas-logo>
+          <div class="mobile-menu-close flex-edge-right">
+            <simple-button
+              target="#"
+              type="inherit round big-icon square"
+              prefixedIcon="close"
+              :aria-label="$t('component_banner_close')"
+              role="button"
+              @click.prevent="hide"
+            ></simple-button>
+          </div>
+        </div>
+        <div class="mobile-menu-main container huge">
+          <header-nav
+            class="vertical"
+            :sites="navigation"
+            :inverted="inverted"
+          ></header-nav>
+          <lang-menu
+            class="flex-edge-right"
+            menu-id="lang-menu-mobile"
+            :inverted="inverted"
+            @buttonBlurred="onBlur"
+          ></lang-menu>
         </div>
       </div>
-      <div class="mobile-menu-main container huge">
-        <header-nav
-          class="vertical"
-          :sites="navigation"
-          :inverted="inverted"
-        ></header-nav>
-        <lang-menu
-          class="flex-edge-right"
-          menu-id="lang-menu-mobile"
-          :inverted="inverted"
-        ></lang-menu>
-      </div>
+      <transition name="fade"
+        ><a
+          class="mobile-menu-backdrop"
+          href="#"
+          tabindex="-1"
+          @click.prevent="hide"
+        ></a
+      ></transition>
     </div>
-    <a class="mobile-menu-backdrop" href="#" tabindex="-1"></a>
-  </div>
+  </transition>
 </template>
 
 <script>
-// TODO Prevent keyboard focus from leaving .the mobile menu while it is opened
+// TODO Fix animation not being shown when mobile menu gets displayed.
 export default {
+  export: ["show", "hide"],
   props: {
     inverted: {
       type: Boolean,
@@ -41,6 +58,27 @@ export default {
     navigation: {
       type: Array,
       required: false,
+    },
+  },
+  data: function () {
+    return {
+      visible: false,
+    };
+  },
+  methods: {
+    onBlur: function () {
+      console.log("Blurred element!");
+      // Trap the focus inside the mobile menu when it is opened, otherwise it can get very confusing.
+      this.$refs.logo.focus();
+    },
+    show: function () {
+      this.visible = true;
+      this.$emit("shown");
+    },
+    hide: function () {
+      this.visible = false;
+      console.log("hide menu");
+      this.$emit("hidden");
     },
   },
 };
@@ -58,7 +96,9 @@ export default {
   bottom: 0;
   font-size: 1.25em;
 
-  &:target {
+  &:target,
+  &:focus-within,
+  &.visible {
     display: block;
   }
 
