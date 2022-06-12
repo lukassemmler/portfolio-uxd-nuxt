b6070888c7bee4e8af6aab868d635d1a03930a19
lukassemmler <kontakt@lukassemmler.de>
2022-06-12 22:14:01 +0200

    Clean up ParallaxBox code and add ideas for future expansion

--- components/ParallaxBox.vue
+++ components/ParallaxBox.vue
@@ -7,7 +7,16 @@
 </template>
 
 <script>
-// Based on https://github.com/apertureless/vue-parallax/blob/master/src/components/Parallax.vue
+// Inspired by https://github.com/apertureless/vue-parallax/blob/master/src/components/Parallax.vue
+/*
+ * Ideas to expand this component:
+ *  - change logic to use intersection observer to detect if element is viewable in viewport
+ *  - add optional prop to make parallax relatve to parent scroll container instead of viewport
+ *  - add optional speed factor for horizontal scrolling (useful to parallax scroll star maps)
+ *  - add optional speed factor easing functions (make parallax scrolling non-linear or take elements arbitrary paths with sinus)
+ *  - add optional clamping so parallaxing only happens inside of the parallax-box instead of everywhere
+ *  - add optional multiple slots which each can parallax at different speed factors (useful for stacking parallaxing frames)
+ */
 export default {
   props: {
     active: {
@@ -15,39 +24,37 @@ export default {
       default: true,
     },
     speedFactor: {
-      // 0 means static (no movement at all)
-      // 1 means going the same speed as the scroll
-      // <0 mean going the opposite direction of the scroll
-      // >0 mean going in the same direction as the scroll
+      /*
+       * 0 means static (no movement at all)
+       * 1 means going the same speed as the scroll
+       * <0 mean going the opposite direction of the scroll
+       * >0 mean going in the same direction as the scroll
+       */
       type: Number,
       default: 0.5,
-    },
-    fixed: {
-      type: Boolean,
-      default: false,
     },
     breakpoint: {
       type: String,
       default: "(min-width: 968px)",
     },
-    scrollStrategy: {
-      // TODO: Implement and rename this prop; this is not about the scroll strategy, it's about changing the 
-      //       neutral position of the parallax scrolling element.
-      // "viewport-center": 
-      //   * The neutral position is reached, when the parent scroll container is scrolled so far, that the parallax box 
-      //     is right in the center of the viewport. 
-      //   * If the speedFactor is 0.5 and you scroll down one viewport height, then the parallax box content will have
-      //     scrolled down half a viewport height.
-      // "scroll-progress"
-      //   * The neutral position is reached, when the parent scroll container is at exactly zero.
-      //   * If the speedFactor is 0.5 and you scroll down 300 pixels, then the parallax box content will have scrolled
-      //     down 150 pixels.
+    scrollAnchor: {
+      /*
+       * "viewport-center":
+       *   - The neutral position is reached, when the parent scroll container is scrolled so far, that the parallax box
+       *     is right in the center of the viewport.
+       *   - If the speedFactor is 0.5 and you scroll down one viewport height, then the parallax box content will have
+       *     scrolled down half a viewport height.
+       * "scroll-top"
+       *   - The neutral position is reached, when the parent scroll container is at exactly 0 scrolling.
+       *   - If the speedFactor is 0.5 and you scroll down 300 pixels, then the parallax box content will have scrolled
+       *     down 150 pixels.
+       */
       type: String,
-      function (value) {
-        return ["viewport-center", "scroll-progress"].includes(value);
+      function(value) {
+        return ["viewport-center", "scroll-top"].includes(value);
       },
-        default: "viewport-center",
-    }
+      default: "viewport-center",
+    },
   },
   data() {
     return {
@@ -57,39 +64,40 @@ export default {
     };
   },
   mounted() {
-    if (!this.active || this.fixed) return;
     this.parallax = this.$refs.parallax;
     this.frame = this.$refs.frame;
-    console.log(this.frame.getBoundingClientRect().top + window.pageYOffset);
-    console.log(document.documentElement.clientHeight);
     this.setupRequestAnimationFrame();
+    //console.log(this.frame.getBoundingClientRect().top + window.pageYOffset);
+    //console.log(document.documentElement.clientHeight);
     this.init();
-    // Run animation once on load, otherwise content will jump till it got scrolled the first time.
-    if (this.active) this.handleScroll();
+    if (!this.active) return;
+    // Run animation once on load, otherwise content will jump when parent gets scrolled the first time.
+    this.handleScroll();
   },
   methods: {
     animateElement() {
-      //const parentHeight = this.frame.offsetHeight;
-      //const parallaxHeight = this.parallax.offsetHeight;
-      //const availableOffset = parallaxHeight - parentHeight;
-      //const offsetY = window.pageYOffset * this.speedFactor;
-
-      const frameViewportOffsetY = this.frame.getBoundingClientRect().top;
-      const offsetViewport =
-        (document.documentElement.clientHeight +
-          this.frame.getBoundingClientRect().height) /
-        2;
-      const offsetY =
-        (frameViewportOffsetY - offsetViewport) * this.speedFactor * -1;
-
-      //if (offsetY <= availableOffset && offsetY >= 0) {
+      let offsetYRaw = 0;
+      switch (this.scrollAnchor) {
+        case "scroll-top":
+          offsetYRaw = window.pageYOffset;
+          break;
+        case "viewport-center":
+          const frameRect = this.frame.getBoundingClientRect();
+          const viewportHeight = document.documentElement.clientHeight;
+          offsetYRaw = (viewportHeight + frameRect.height) / 2 - frameRect.top;
+          break;
+        default:
+          throw new Error(
+            `Unhandled scroll anchor type '${this.scrollAnchor}'. `
+          );
+      }
+      const offsetY = offsetYRaw * this.speedFactor;
       this.parallax.style.transform = `translate3d(0, ${offsetY}px ,0)`;
-      console.log("parallaxing!");
-      //}
+      //console.log("parallaxing!");
     },
     handleScroll() {
+      if (!this.active || !this.isInView(this.parallax)) return;
       window.requestAnimationFrame(() => {
-        if (!this.isInView(this.parallax)) return;
         this.animateElement();
       });
     },
@@ -101,12 +109,19 @@ export default {
           (window.innerHeight || document.documentElement.clientHeight)
       );
     },
+    /*
+    parallaxIsInsideBox() {
+      // Untested method, should be used later for clamping
+      const parentHeight = this.frame.offsetHeight;
+      const parallaxHeight = this.parallax.offsetHeight;
+      const availableOffset = parallaxHeight - parentHeight;
+      return offsetY <= availableOffset && offsetY >= 0;
+    },
+    */
     setupListener() {
-      if (this.mediaQuery.matches) {
+      if (this.mediaQuery.matches)
         window.addEventListener("scroll", this.handleScroll, false);
-      } else {
-        window.removeEventListener("scroll", this.handleScroll, false);
-      }
+      else window.removeEventListener("scroll", this.handleScroll, false);
     },
     init() {
       this.mediaQuery = window.matchMedia(this.breakpoint);
@@ -134,7 +149,9 @@ export default {
 
 <style lang="scss" scoped>
 .parallax-box {
-  background-color: yellow;
+  &.debug {
+    background-color: yellow;
+  }
 }
 .parallax-box-content {
   position: relative;
