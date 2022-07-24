d88b3d15bf1282ae3ab47fa053f1a585debecd14
lukassemmler <kontakt@lukassemmler.de>
2022-07-24 14:19:07 +0200

    Add content to page '3d-modelling'

--- pages/projects/3d-modelling.vue
+++ pages/projects/3d-modelling.vue
@@ -1,13 +1,79 @@
 <template>
-  <h1>3D-Modelling</h1>
+  <project-template
+    background="bg-gradient-pink"
+    :title="$t('meta-title_3d-modelling')"
+    :meta-name="$t('project-summary-name_3d-modelling')"
+    :meta-tasks="$t('project-summary-tasks_3d-modelling')"
+    :meta-format="$t('project-summary-format_3d-modelling')"
+    :meta-lecture="$t('project-summary-client_3d-modelling')"
+    :meta-date="$t('project-summary-date_3d-modelling')"
+    :project-tools="[{ iconId: 'Blender', label: 'Blender' }]"
+    :nav-previous-title="$t('project-nav_design-booklet')"
+    :nav-previous-link="'projects/design-booklet'"
+    :nav-previous-color="'bg-hover-gray'"
+    :nav-next-title="$t('project-nav_steering-wheel')"
+    :nav-next-link="'projects/steering-wheel'"
+    :nav-next-color="'bg-hover-green'"
+  >
+    <template v-slot:header>
+      <!-- TODO: Add Sculptris as project tool -->
+      <div class="container huge">
+        <div class="pillar-container gapped-v gapped-h">
+          <div class="pillar-row">
+            <div class="pillar-col">
+              <simple-image
+                class="rounded"
+                src="3d-modelling/Donuts.jpg"
+                :alt="$t('alt_3d-modelling_donuts')"
+                ratio="16-by-9"
+                sizes="xs:400px sm:640px md:960px lg:1200px"
+              ></simple-image>
+            </div>
+          </div>
+          <div class="pillar-row">
+            <div class="pillar-col">
+              <simple-image
+                class="rounded"
+                src="3d-modelling/Man Bust.jpg"
+                :alt="$t('alt_3d-modelling_man-bust')"
+                ratio="16-by-9"
+                sizes="xs:400px sm:640px md:960px lg:1200px"
+              ></simple-image>
+            </div>
+          </div>
+          <div class="pillar-row">
+            <div class="pillar-col">
+              <simple-image
+                class="rounded"
+                src="3d-modelling/Light Bulbs.jpg"
+                :alt="$t('alt_3d-modelling_light-bulbs')"
+                ratio="16-by-9"
+                sizes="xs:400px sm:640px md:960px lg:1200px"
+              ></simple-image>
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+    <template v-slot:content>
+      <section id="section-overview" class="section">
+        <div class="container small">
+          <h2 id="overview" class="sr-only">{{ $t("heading_overview") }}</h2>
+          <p v-html="$t('text_3d-modelling_general').join('')"></p>
+        </div>
+      </section>
+    </template>
+  </project-template>
 </template>
 
 <script>
+import ProjectTemplate from "~/components/ProjectTemplate.vue";
 export default {
-
-}
+  components: {
+    ProjectTemplate,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
