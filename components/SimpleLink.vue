ce93888732d31f17c1103b6a2f3b1078a0dd56a6
lukassemmler <kontakt@lukassemmler.de>
2022-12-10 20:43:15 +0100

    Fix SimpleLink not passing click events

--- components/SimpleLink.vue
+++ components/SimpleLink.vue
@@ -8,10 +8,25 @@ export default {
     if (this.routed)
       return h(
         "nuxt-link",
-        { props: { to: this.target } },
+        {
+          props: { to: this.target },
+          on: { click: this.onClick },
+        },
         this.$slots.default
       );
-    return h("a", { attrs: { href: this.target } }, this.$slots.default);
+    return h(
+      "a",
+      {
+        attrs: { href: this.target },
+        on: { click: this.onClick },
+      },
+      this.$slots.default
+    );
+  },
+  methods: {
+    onClick: function (event) {
+      this.$emit('click', event);
+    },
   },
 };
 </script>
