813e6b8222bf4421df3e7ac9b54cc395513a1416
lukassemmler <kontakt@lukassemmler.de>
2022-12-10 20:36:30 +0100

    Add component 'SimpleLink'

---
+++ components/SimpleLink.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  props: {
+    target: { type: String },
+    routed: { type: Boolean, default: true },
+  },
+  render(h) {
+    if (this.routed)
+      return h(
+        "nuxt-link",
+        { props: { to: this.target } },
+        this.$slots.default
+      );
+    return h("a", { attrs: { href: this.target } }, this.$slots.default);
+  },
+};
+</script>
+
+<style>
+</style>
