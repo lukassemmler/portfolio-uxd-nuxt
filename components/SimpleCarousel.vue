2a170a614495e334a4999a054fc631048e5e03ba
lukassemmler <kontakt@lukassemmler.de>
2023-02-25 19:24:08 +0100

    Add component 'SimpleCarousel'

---
+++ components/SimpleCarousel.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="simple-carousel">
+    <div class="simple-carousel-body">
+      <div class="simple-carousel-content">
+        <slot>
+          <!-- Intended to include 'column-card' components -->
+          Completely empty.
+        </slot>
+      </div>
+      <div class="simple-carousel-footer">
+        <div class="simple-carousel-footer-content">
+          <div v-if="showPagination" class="simple-carousel-pagination">
+            <ol class="simple-carousel-pagination-list">
+              <li v-for="index in numberOfPages" :key="index">
+                <simple-button
+                  class="simple-carousel-pagination-button"
+                  type="invisible-shadow"
+                  :aria-selected="displayedIndex === index"
+                  ><span class="sr-only">{{ $t("label_step") }} </span
+                  >{{ index }}</simple-button
+                >
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SimpleButton from "./SimpleButton.vue";
+export default {
+  components: { SimpleButton },
+  props: {
+    showPagination: {
+      type: Boolean,
+      default: true,
+    },
+    numberOfPages: {
+      type: Number,
+      default: 1,
+    },
+    displayedIndex: {
+      type: Number,
+      default: 1,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.simple-carousel {
+  width: 100%;
+  margin-bottom: 2em;
+  padding-left: 1em;
+  padding-right: 1em;
+
+  .simple-carousel-body {
+    position: relative;
+    max-width: $max-size-bulge;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .simple-carousel-content {
+    display: flex;
+    flex-flow: row nowrap;
+
+    & > * {
+      width: $max-size-wrapper;
+    }
+
+    & > *:not(:last-child) {
+      margin-right: 1em;
+    }
+  }
+
+  .simple-carousel-footer {
+    max-width: $max-size-bulge;
+  }
+
+  .simple-carousel-footer-content {
+    width: calc(100% - 4em); // 
+    box-sizing: border-box;
+    padding: 0.3em;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+
+    & > *:not(:last-child) {
+      margin-right: 0.3em;
+    }
+  }
+
+  &.interactive {
+    .simple-carousel-content > * {
+      margin-bottom: 0;
+    }
+    .simple-carousel-footer {
+      display: block;
+    }
+  }
+
+  .simple-carousel-pagination-list {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: row;
+
+    & > *:not(:last-child) {
+
+    }
+  }
+
+  @media screen and (min-width: $breakpoint-semi-big) {
+    .simple-carousel-footer-content {
+      width: calc(100% - 12em); // ratio of side card
+    }
+  }
+}
+</style>
