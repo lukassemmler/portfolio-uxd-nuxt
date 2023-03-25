e48417ac806ce5da7154fe22bb32ef04587bb237
lukassemmler <kontakt@lukassemmler.de>
2023-03-25 18:51:40 +0100

    Change SimpleLink to have inactive state

--- components/SimpleLink.vue
+++ components/SimpleLink.vue
@@ -3,8 +3,17 @@ export default {
   props: {
     target: { type: String },
     routed: { type: Boolean, default: true },
+    inactive: { type: Boolean },
   },
   render(h) {
+    if (this.inactive)
+      return h(
+        "span",
+        {
+          on: { click: this.onClick },
+        },
+        this.$slots.default
+      );
     if (this.routed)
       return h(
         "nuxt-link",
@@ -25,7 +34,7 @@ export default {
   },
   methods: {
     onClick: function (event) {
-      this.$emit('click', event);
+      this.$emit("click", event);
     },
   },
 };
