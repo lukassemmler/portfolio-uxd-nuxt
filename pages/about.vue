cab94cb2ec4752516933b032488e5648e89a26a9
lukassemmler <kontakt@lukassemmler.de>
2023-06-16 21:07:01 +0200

    Add notes to 'about' page

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="container huge">
+    <h2>Presentation</h2>
+
+    <h3>Things I <em>like</em></h3>
+    <ul>
+      <li>UI design</li>
+      <li>illustration</li>
+      <li>frontend coding</li>
+      <li>game development</li>
+    </ul>
+
+    <h3>What I can <em>offer</em></h3>
+    <ul>
+      <li>friendly and reliable</li>
+      <li>small ideas and questions</li>
+      <li>well-structured UI design</li>
+      <li>robust concepts and design</li>
+    </ul>
+
+    <h3>Things I'm <em>good</em> at</h3>
+    <ul>
+      <li>big projects with high complexity</li>
+      <li>organizing information (wikis and documentation)</li>
+      <li>design following accessability guidelines</li>
+      <li>automation of unefficient work steps</li>
+    </ul>
+
+    <h3>Things I'm <em>bad</em> at</h3>
+    <ul>
+      <li>quick-fire projects</li>
+      <li>dealing with uncertainty</li>
+      <li>straight-forward, repetitive tasks</li>
+    </ul>
+
     <div class="pillar-container gapped-h">
       <div class="pillar-row">
         <section class="pillar-col-bg-6">
@@ -13,9 +46,7 @@
             :subtitle="$t('text_about_work-experience-3_title')"
             :timespan="$t('text_about_work-experience-3_time-span')"
             :location="$t('text_about_work-experience-3_location')"
-            :description="
-              $t('text_about_work-experience-3_description').join('')
-            "
+            :description="$t('text_about_work-experience-3_description').join('')"
           ></cv-record>
           <cv-record
             iconId="business"
@@ -24,9 +55,7 @@
             :subtitle="$t('text_about_work-experience-2_title')"
             :timespan="$t('text_about_work-experience-2_time-span')"
             :location="$t('text_about_work-experience-2_location')"
-            :description="
-              $t('text_about_work-experience-2_description').join('')
-            "
+            :description="$t('text_about_work-experience-2_description').join('')"
           ></cv-record>
           <cv-record
             iconId="business"
@@ -35,9 +64,7 @@
             :subtitle="$t('text_about_work-experience-1_title')"
             :timespan="$t('text_about_work-experience-1_time-span')"
             :location="$t('text_about_work-experience-1_location')"
-            :description="
-              $t('text_about_work-experience-1_description').join('')
-            "
+            :description="$t('text_about_work-experience-1_description').join('')"
           ></cv-record>
         </section>
         <section class="pillar-col-bg-6">
@@ -60,9 +87,7 @@
             :location="$t('text_about_education-2_location')"
             :description="$t('text_about_education-2_description').join('')"
             ><template v-slot:extra>
-              <a href="download/BA Lukas Semmler.pdf" class="link-button"
-                >&#8594;PDF</a
-              >
+              <a href="download/BA Lukas Semmler.pdf" class="link-button">&#8594;PDF</a>
             </template></cv-record
           >
           <cv-record
