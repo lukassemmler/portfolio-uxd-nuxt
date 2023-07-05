a9d244f9c660754bef3a519ff485430f89c5b636
lukassemmler <kontakt@lukassemmler.de>
2023-07-05 17:09:39 +0200

    Add script to get usage of locales

---
+++ bin/locales-usage.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+const path = require('path');
+const {Color} = require(path.resolve(__filename + './../../assets/lib/color'));
+
+function readJsonFilesInDir(dir, onJson = () => { }) {
+  const files = fs.readdirSync(dir);
+  for (const file of files) {
+    const fileName = path.extname(file).toLowerCase();
+    if (fileName !== '.json') continue;
+    const filePath = path.join(dir, file);
+    const content = fs.readFileSync(filePath, 'utf8');
+    const json = JSON.parse(content);
+    onJson({ json, file, path: filePath });
+  }
+}
+
+function readFilesInDirRecursively(dir, onFile = () => { }, whitelist = []) {
+  const files = fs.readdirSync(dir);
+  for (const file of files) {
+    const fileName = path.extname(file).toLowerCase();
+    if (whitelist.length > 0 && !whitelist.includes(fileName)) continue;
+    const filePath = path.join(dir, file);
+    const stats = fs.statSync(filePath);
+    if (stats.isDirectory()) {
+      readFilesInDirRecursively(filePath, onFile, whitelist);
+      continue;
+    }
+    const content = fs.readFileSync(filePath, 'utf8');
+    onFile({ content, file, path: filePath });
+  }
+}
+
+const getAbsolutePath = (...relPath) => path.resolve(__filename, "../", ...relPath);
+
+// Step 1: Load locale definitions
+const localeDirRaw = '../locales';
+const localeDir = getAbsolutePath(localeDirRaw);
+const definitons = new Set();
+readJsonFilesInDir(localeDir, ({ json: locale }) => {
+  const keys = Object.keys(locale);
+  keys.forEach(key => definitons.add(key));
+});
+console.log(definitons.size)
+
+// Step 2: Get occurrences in files
+const targetDirsRaw = [
+  '../pages',
+  '../components',
+  '../layouts',
+  //'../assets/data',
+];
+const targetDirs = targetDirsRaw.map(dir => getAbsolutePath(dir));
+//console.log(targetDirs);
+const occurrences = {};
+for (const dir of targetDirs)
+readFilesInDirRecursively(dir, ({content, file, path}) => {
+  const matches = content.matchAll(/\$t\(['"]([^'"]+?)['"]\)/g);
+  const ids = Array.from(matches).map(match => match[1]);
+  const uniqueIds = new Set(ids);
+  occurrences[path] = Array.from(uniqueIds.values()).sort();
+});
+console.log(occurrences);
+// TODO: Hande 'nav.json'
+
+// Step 3: Check if all definitions are used
+const usedIds = new Set();
+const undefinedIds = new Set();
+for (const file in occurrences) {
+  const occurrencesPerFile = occurrences[file];
+  for (const id of occurrencesPerFile) {
+    if (definitons.has(id)) {
+      usedIds.add(id);
+      continue;
+    }
+    undefinedIds.add(id);
+  }
+}
+const unusedIds = [];
+const definitionsAsArray = Array.from(definitons.values());
+for (const definition of definitionsAsArray) {
+  if (usedIds.has(definition)) continue;
+  unusedIds.push(definition);
+}
+const stats = {
+  usedIds: Array.from(usedIds.values()).sort(),
+  unusedIds: unusedIds,
+  undefinedIds: Array.from(undefinedIds.values()).sort(),
+};
+console.log(stats);
