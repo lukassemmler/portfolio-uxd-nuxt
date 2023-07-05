ce951012f900d47255ba5e019ec73124677fd441
lukassemmler <kontakt@lukassemmler.de>
2023-07-05 13:37:56 +0200

    Add module for console colors

---
+++ assets/lib/color.js
@@ -0,0 +1,15 @@
+Color = Object.freeze({
+  Reset: '\x1b[0m',
+  Red: '\x1b[31m',
+  Green: '\x1b[32m',
+  Yellow: '\x1b[33m',
+  Blue: '\x1b[34m',
+  BgRed: '\x1b[41m',
+  BgGreen: '\x1b[42m',
+  BgYellow: '\x1b[43m',
+  BgBlue: '\x1b[44m',
+});
+
+module.exports = {
+  Color,
+}
