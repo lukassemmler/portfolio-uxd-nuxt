d3474a023fa2748318e2290a98abee6ad021a0cf
lukassemmler <kontakt@lukassemmler.de>
2023-07-11 16:28:19 +0200

    Change layout 'vanilla' to get meta from nav.json

--- layouts/vanilla.vue
+++ layouts/vanilla.vue
@@ -8,25 +8,62 @@
 
 <script>
 import navigation from "assets/data/nav.json";
+import { getMetaData } from "~/assets/lib/meta-data";
 
-const { pages } = navigation;
-const pagesByPath = new Map();
-console.log(pages)
-//pages.forEach(page => pagesByPath.set(page.path, page));
-for (const page of pages) {
-  console.log(page.path)
-  pagesByPath.set(page.path, page);
+function getPagesByPath(pages) {
+  const pagesByPath = new Map();
+  pages.forEach((page) => pagesByPath.set(page.path, page));
+  for (const page of pages)
+    pagesByPath.set(page.path, page);
+  return pagesByPath;
 }
-  console.log(pagesByPath.keys())
 
 export default {
   layout: "default",
-  mounted() {
+  data: function () {
+    return {
+      pageTitle: null,
+      metaDescription: null,
+      metaKeywords: null,
+      metaAuthor: null,
+      metaColor: null,
+      thumbnailSrc: null,
+      thumbnailAlt: null,
+    };
+  },
+  created() {
+    const { pages } = navigation;
+    const pagesByPath = getPagesByPath(pages);
     const route = this.$route.path;
-    const page = pagesByPath.get(route);
-    console.log(route)
-  if (!page) return;
-  console.log(this.$t(page.titleId));
+    const routePage = pagesByPath.get(route);
+    if (!routePage)
+      console.warn(`Could not find page meta data for route '${route}'. `);
+    const page = routePage ?? {};
+    //console.log("route:", route);
+    //console.log("title id:", page);
+    //console.log("title id:", this.$t(page.titleId));
+    const { titleId, metaAuthorId, metaDescriptionId, metaKeywordsId, metaColor, thumbnailSrc, thumbnailAltId } = page;
+    this.pageTitle = titleId ? `${this.$t(titleId)} \u2013 Lukas Semmler` : "Lukas Semmler";
+    this.metaDescription = metaAuthorId;
+    this.metaKeywords = metaDescriptionId;
+    this.metaAuthor = metaKeywordsId;
+    this.metaColor = metaColor ?? "#4a4a4a";
+    this.thumbnailSrc = thumbnailSrc ?? "/thumbnail/Thumbnail Design Booklet.jpg";
+    this.thumbnailAlt = thumbnailAltId ?? "alt_thumbnail_general";
+  },
+  head() {
+    return getMetaData({
+      url: this.$config.baseUrl + this.$route.path,
+      siteName: this.$t("label_site-name"),
+      title: this.pageTitle,
+      titleTemplate: "%s",
+      description: this.$t(this.metaDescription),
+      keywords: this.$t(this.metaKeywords),
+      author: this.$t(this.metaAuthor),
+      thumbnail: this.thumbnailSrc,
+      thumbnailAlt: this.$t(this.thumbnailAlt),
+      themeColor: this.metaColor,
+    });
   },
 };
 </script>
